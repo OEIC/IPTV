2f035bd2af51633ea7b073611884851cc1b1ceb0
OEIC <silvernoma_cn@163.com>
2024-07-05 23:01:43 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -9,18 +9,18 @@ favorite_page_num = 5
 default_page_num = 3
 urls_limit = 15
 open_sort = True
-response_time_weight = 0.5
-resolution_weight = 0.5
+response_time_weight = 0.3
+resolution_weight = 0.7
 recent_days = 30
 ipv_type = "all"
 domain_blacklist = []
 url_keywords_blacklist = []
 open_subscribe = True
 subscribe_urls = [
+    "https://freetv.fun/test_channels_china_new.txt",
     "https://m3u.ibert.me/txt/fmml_dv6.txt",
     "https://m3u.ibert.me/txt/o_cn.txt",
     "https://m3u.ibert.me/txt/j_iptv.txt",
-    "https://freetv.fun/test_channels_china_new.txt",
 ]
 open_multicast = True
 region_list = ["all"]
